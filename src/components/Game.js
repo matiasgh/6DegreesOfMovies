163610d6d8de42160e3b44a19cae7a000a1b4ee3
matiasgr <matiasgr@uio.no>
2021-09-06 14:47:38 +0200

    Added Game Over when user finished the game

--- src/components/Game.js
+++ src/components/Game.js
@@ -17,6 +17,7 @@ export default function Game() {
     const[renderMovie, setRenderMovie] = useState(false)
     const[degree, setDegree] = useState(0)
     const[userPath, setUserPath] = useState([])
+    const[gameOver, setGameOver] = useState(false)
 
     const history = useHistory()
 
@@ -34,15 +35,27 @@ export default function Game() {
     useEffect(() => {
         graph.setRandom()
         let paths = (graph.findOptimal())
-        while(paths == null){
+        while(paths == null && paths.length <= 4){
             graph.setRandom()
             paths = (graph.findOptimal())
         }
+        console.log(paths)
         setSolution(structurePath(paths))
         setCurrentActor(graph.startActor)
         setLoading(false)
-        console.log(allMovies)
     },[])
+
+    useEffect(() => {
+        if(currentActor != null){
+            const movies = Array.from(currentActor.movies)
+            for (let i = 0; i < movies.length; i++) {
+                if(graph.endActor.movies.has(movies[i])){
+                    console.log("Yayy, game over!")
+                    setGameOver(true)
+                }
+            }
+        }
+    },[currentActor])
 
     function startGame(){
         history.push("/game")
@@ -75,16 +88,28 @@ export default function Game() {
                 <Button variant="dark" className="w-25" onClick={startGame}>Start Game</Button>
             </Row>
             <h4> <BiArrowBack style={{color:"blue", cursor: "pointer"}} onClick={goHome}/> Back</h4>
-            {!loading && <Container fluid>
-                <h4 style={{color: "black", display: 'flex',  justifyContent:'center', alignItems:'center'}}>Moves: {degree}</h4>
-                <br/>
-                <h4 style={{color: "grey", display: 'flex',  justifyContent:'center', alignItems:'center'}}>Current {renderMovie ? "Movie: " + currentMovie.name : "Actor: " + currentActor.name}</h4>
+            {!loading && !gameOver && <Container fluid style={{maxWidth: "1000px"}}>
+                <Row>
+                    <Col className = "text-center">
+                        <h5>Start actor:</h5>
+                        {graph.startActor.name}
+                    </Col>
+                    <Col className = "text-center">
+                        <h4 style={{color: "black", display: 'flex',  justifyContent:'center', alignItems:'center'}}>Moves: {degree}</h4>
+                        <br/>
+                        <h4 style={{color: "grey", display: 'flex',  justifyContent:'center', alignItems:'center'}}>Current {renderMovie ? "Movie: " + currentMovie.name : "Actor: " + currentActor.name}</h4>
+                    </Col>
+                    <Col className = "text-center">
+                        <h5>Actor to find:</h5>
+                        {graph.endActor.name}
+                    </Col>
+                </Row>
             </Container>}
-            {!loading && <Container className="d-flex align-items-center justify-content-center mb-5">
+            {!loading && !gameOver && <Container className="d-flex align-items-center justify-content-center mb-5">
                 <Card style ={{maxWidth: "1000px", minWidth: "400px", maxHeight: "300px", marginBottom:"50px"}} class="overflow-auto">
                     <div className="overflow-auto">
                     <ListGroup>
-                        {!renderMovie && Array.from(currentActor.movies).map((movie) => (
+                        {!renderMovie && Array.from(currentActor.movies).sort().map((movie) => (
                         <ListGroup.Item action onClick={() => goMovie(movie.id)}> 
                             <h6>
                                 <Row>
@@ -104,7 +129,7 @@ export default function Game() {
                         ))}
                     </ListGroup>
                     <ListGroup>
-                        {renderMovie && Array.from(currentMovie.actors).map((actor) => (
+                        {renderMovie && Array.from(currentMovie.actors).sort().map((actor) => (
                         <ListGroup.Item action onClick={() => goActor(actor.id)}>
                             <h6>
                                 <FaTheaterMasks style={{color:"black"}} style={{marginRight:"20px"}}/>  
@@ -116,6 +141,12 @@ export default function Game() {
                     </div>
                 </Card>
             </Container>}
+            <Container className="d-flex align-items-center justify-content-center">
+                {gameOver && <Card bg-secondary className="d-flex align-items-center justify-content-center mt-5" style={{maxWidth: "600px"}}>
+                    <h2>Game Over</h2>
+                    <h4 class="text-success">Congratulations!</h4>
+                </Card>}
+            </Container>
         </Container>
     )
 }
